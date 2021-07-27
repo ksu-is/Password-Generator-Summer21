d9a189e50e191b98335c80d517417c75ce3e96f3
hunterh77 <85526397+hunterh77@users.noreply.github.com>
2021-07-26 22:06:46 -0400

    Code to Write File
    This code creates a text file and creates the make password function so it will be callable in the addpassword function.

--- SharedCode.py
+++ SharedCode.py
@@ -30,3 +30,36 @@ password = "".join(temp)
 
 #print the password
 print(password)
+
+def makepassword(length):
+  lower = string.ascii_lowercase
+  upper = string.ascii_uppercase
+  num = string.digits
+  symbols = string.punctuation
+  all = lower + upper + num + symbols
+  temp = random.sample(all,length)
+  password = "".join(temp)
+  return password
+
+try:
+    newfile = open("passwordfile.txt","x")
+except:
+    newfile = open("passwordfile.txt", "w")
+
+
+def addpassword():
+    websitename = input("Please enter the website that this password is for: ")
+    username = input("Please enter the username this password will be associated with: ")
+    length = int(input("How long should this password be? "))
+    password = makepassword(length)
+    newfile = open("passwordfile.txt", "w")
+    newfile.write("Website: " + websitename)
+    newfile.write("\t")
+    newfile.write("Username: " + username)
+    newfile.write("\t") 
+    newfile.write("Password: " + password)
+    newfile.close()
+
+
+addpassword()
+
