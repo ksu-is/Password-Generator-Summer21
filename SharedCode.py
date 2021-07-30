433bb633b01cb72f26d3966cd9a91a728603dabd
hunterh77 <85526397+hunterh77@users.noreply.github.com>
2021-07-30 11:26:25 -0400

    GUI
    Adding GUI, text boxes, and updating the flow of the code

--- SharedCode.py
+++ SharedCode.py
@@ -9,57 +9,77 @@ import string
 
 print('hello, Welcome to Password generator!')
 
-#input the length of password
-length = int(input('\nEnter the length of password: '))                      
-
-#define data
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-num = string.digits
-symbols = string.punctuation
-#string.ascii_letters
-
-#combine the data
-all = lower + upper + num + symbols
-
-#use random 
-temp = random.sample(all,length)
-
-#create the password 
-password = "".join(temp)
-
-#print the password
-print(password)
-
-def makepassword(length):
-  lower = string.ascii_lowercase
-  upper = string.ascii_uppercase
-  num = string.digits
-  symbols = string.punctuation
-  all = lower + upper + num + symbols
-  temp = random.sample(all,length)
-  password = "".join(temp)
-  return password
-
 try:
     newfile = open("passwordfile.txt","x")
 except:
     newfile = open("passwordfile.txt", "w")
 
 
-def addpassword():
-    websitename = input("Please enter the website that this password is for: ")
-    username = input("Please enter the username this password will be associated with: ")
-    length = int(input("How long should this password be? "))
+
+    
+def makepassword(length):
+    length = int(length)
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    num = string.digits
+    symbols = string.punctuation
+    all = lower + upper + num + symbols
+    temp = random.sample(all,length)
+    password = "".join(temp)
+    return password
+    
+def addpassword(website, username, length):
+     
     password = makepassword(length)
-    newfile = open("passwordfile.txt", "w")
-    newfile.write("Website: " + websitename)
+    newfile = open("passwordfile.txt", "a")
+    newfile.write("\n")
+    newfile.write("Website: " + website)
     newfile.write("\t")
     newfile.write("Username: " + username)
     newfile.write("\t") 
     newfile.write("Password: " + password)
-    newfile.close()
+    
 
 
-addpassword()
+import tkinter as tk
 
+root = tk.Tk()
+root.title("Password Generator")
+canvas = tk.Canvas(root, height=500, width=500)
+canvas.pack()
+
+frame = tk.Frame(root, bg="#9196c0", bd=5)
+frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
+
+frame1 = tk.Frame(root, bg="#9196c0", bd=5)
+frame1.place(relx=0.5, rely=0.2, relwidth=0.75, relheight= 0.1, anchor='n')
+
+frame2 = tk.Frame(root, bg="#9196c0", bd=5)
+frame2.place(relx=0.5, rely=0.3, relwidth=0.75, relheight= 0.1, anchor='n')
+
+frame3 = tk.Frame(root, bg="#9196c0", bd=5)
+frame3.place(relx=0.5, rely=0.4, relwidth=0.75, relheight= 0.1, anchor='n')
+
+website_label = tk.Label(text = "Website", bg="#9196c0", font=40).place(x = 300, y = 65)
+username_label = tk.Label(text = "Username", bg="#9196c0",font=40).place(x = 300, y = 115)
+length_label = tk.Label(text = "Length", bg="#9196c0",font=40).place(x = 300, y = 165)
+
+
+
+
+entry = tk.Entry(frame, font=40)
+
+entry.place(relwidth=0.5, relheight=1)
+
+entry1 = tk.Entry(frame1, font=40)
+
+entry1.place(relwidth=0.5, relheight=1)
+
+entry2 = tk.Entry(frame2, font=40)
+
+entry2.place(relwidth=0.5, relheight=1)
+
+button = tk.Button(frame3, text="AddPassword", font=40, command=lambda: addpassword(entry.get(),entry1.get(),entry2.get()))
+button.place(relx=0, relheight=1, relwidth=0.3)
+
+root.mainloop()
